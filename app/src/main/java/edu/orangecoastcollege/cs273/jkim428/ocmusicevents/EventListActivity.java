d8f94f79e9966af86cd5bb8b51065c1747e542dc
jkim428 <jkim428@student.cccd.edu>
2017-09-21 15:48:18 -0700

    First pass done.

--- app/src/main/java/edu/orangecoastcollege/cs273/jkim428/ocmusicevents/EventListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/jkim428/ocmusicevents/EventListActivity.java
@@ -1,13 +1,35 @@
 package edu.orangecoastcollege.cs273.jkim428.ocmusicevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventListActivity extends AppCompatActivity {
+public class EventListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
+        //Content has already been set for ListActivity
+        // setContentView(R.layout.activity_event_list);
+
+        // Define a built-in list adapter for this ListActivity:
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        // Use the position in the list to look up in the titles array
+        String title = MusicEvent.titles[position];
+        String details = MusicEvent.details[position];
+
+        // Create an Intent to go to DetailsActivity with title and details sent
+        Intent detailsIntent = new Intent(this, EventDetailsActivity.class);
+        detailsIntent.putExtra("title", title);
+        detailsIntent.putExtra("details", details);
+        startActivity(detailsIntent);
     }
 }
